8a2217756774f96671068a3dee31035768d6ef3b
Vladan Duronjic <48985722+vladan1996@users.noreply.github.com>
2022-12-07 15:43:35 +0100

    Add files via upload

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,47 +1,115 @@
- import React from 'react';
+ import {useEffect, useState} from 'react';
  import {Week, TimelineViews, TimelineMonth, Day, ScheduleComponent, ViewsDirective, ViewDirective, ResourcesDirective, ResourceDirective, Inject } from '@syncfusion/ej2-react-schedule';
  import useData from './useData';
  import './Home.css'
  import { Editing } from 'devextreme-react/data-grid';
 import { DateTimePickerComponent } from '@syncfusion/ej2-react-calendars';
 import { MultiSelectComponent } from '@syncfusion/ej2-react-dropdowns';
+import {L10n} from '@syncfusion/ej2-base';
+import {insertJob,deleteJob, updateJob} from './homeService';
+
+
+
+
 
  const Home = () => {
 
    const {data} = useData();
 
+   const customer = data.customers;
+   const docks = data.docks;
+   const statuses = data.statuses;
+
+   
+console.log(customer);
+console.log(statuses);
+
+   const fieldsDocks = { value: 'Id', text: 'DockName' };
+   const fieldsCustomer = { value: 'Id', text: 'FirstName'};
+   const fieldsStatuses = { value: 'Id', text: 'Name' };
+
+
+function onActionBegin(args){
+  console.log(args);
+
+  if(args.requestType === 'eventCreate') {
+
+    const job = {
+      CustomerId: args.addedRecords[0].CustomerId[0],
+      SuportStatusId: args.addedRecords[0].StatusId[0],
+      DockId: args.addedRecords[0].OwnerId,
+      endTime : args.addedRecords[0].EndTime,
+      startTime: args.addedRecords[0].StartTime,
+      NoPallets: parseInt(args.addedRecords[0].NoPallets),
+      LoadNo: args.addedRecords[0].LoadNo,
+      LoadType: args.addedRecords[0].LoadType,
+      Customers: [],
+      SupportStatuses: [],
+      Dock: [],
+      job:[]
+    }
+    insertJob(job);
+    console.log(args);
+  }else if(args.requestType === 'eventRemove'){
+    deleteJob(args.deletedRecords[0].Guid);
+  }
+}
+
+
 
  function editorTemplate(props) {
-            return (props !== undefined && Object.keys(props).length > 0 ? <table className="custom-event-editor" style={{ width: '100%', padding: '5' }}><tbody>
-          <tr><td className="e-textlabel">Summary</td><td colSpan={4}>
-            <input id="Summary" className="e-field e-input" type="text" name="Subject" style={{ width: '100%' }}/>
+  console.log(props)
+  
+       return (props !== undefined && Object.keys(props).length > 0 ? 
+       <table className="custom-event-editor" style={{ width: '100%', padding: '5' }}><tbody>
+              <tr><td className="e-textlabel">Start Date</td><td colSpan={4}>
+            <DateTimePickerComponent format='dd/MM/yy hh:mm' id="StartTime" data-name="StartTime" className="e-field"></DateTimePickerComponent>
+                    {/* value={new Date(props.startTime || props.StartTime)} */}
           </td></tr>
-          <tr><td className="e-textlabel">Owner</td><td colSpan={4}>
-            {/* <MultiSelectComponent className="e-field" placeholder='Choose owner' data-name="OwnerId" dataSource={data} fields={this.fields} value={props.OwnerId}/> */}
+
+          <tr><td className="e-textlabel">End Date</td><td colSpan={4}>
+            <DateTimePickerComponent format='dd/MM/yy hh:mm' id="EndTime" data-name="EndTime" value={new Date(props.endTime || props.EndTime)} className="e-field"></DateTimePickerComponent>
+         {/* value={new Date(props.endTime || props.EndTime)} */}
           </td></tr>
-          <tr><td className="e-textlabel">From</td><td colSpan={4}>
-            {/* <DateTimePickerComponent format='dd/MM/yy hh:mm a' id="StartTime" data-name="StartTime" value={new Date(props.startTime || props.StartTime)} className="e-field"></DateTimePickerComponent> */}
+
+          <tr><td className="e-textlabel">Dock</td><td colSpan={4}>
+            <MultiSelectComponent className="e-field dock" placeholder='Choose owner' data-name="OwnerId" dataSource={docks} fields={fieldsDocks} onChange={e => console.log(e)}/>
           </td></tr>
-          <tr><td className="e-textlabel">To</td><td colSpan={4}>
-            {/* <DateTimePickerComponent format='dd/MM/yy hh:mm a' id="EndTime" data-name="EndTime" value={new Date(props.endTime || props.EndTime)} className="e-field"></DateTimePickerComponent> */}
+
+          <tr><td className="e-textlabel">Customer</td><td colSpan={4}>
+            <MultiSelectComponent className="e-field" placeholder='Choose Customer' data-name="CustomerId" dataSource={customer} fields={fieldsCustomer}  />
           </td></tr>
-          <tr><td className="e-textlabel">Reason</td><td colSpan={4}>
-            <textarea id="Description" className="e-field e-input" name="Description" rows={3} cols={50} style={{ width: '100%', height: '60px !important', resize: 'vertical' }}></textarea>
-          </td></tr></tbody></table> : <div></div>);
+
+          <tr><td className="e-textlabel">Status</td><td colSpan={4}>
+            <MultiSelectComponent className="e-field" placeholder='Choose Status' data-name="StatusId" dataSource={statuses} fields={fieldsStatuses}/>
+          </td></tr>
+
+          <tr><td className="e-textlabel">Load Type</td><td colSpan={4}>
+            <input id="LoadType" className="e-field e-input ltype" type="text" name="LoadType" style={{ width: '100%' }}/>
+          </td></tr>
+
+          <tr><td className="e-textlabel">Load No.</td><td colSpan={4}>
+            <input id="LoadNo" className="e-field e-input" type="text" name="LoadNo" style={{ width: '100%' }}/>
+          </td></tr>
+       
+          <tr><td className="e-textlabel">Number of Pallets</td><td colSpan={4}>
+            <input id="NoPallets" className="e-field e-input" type="number" name="NoPallets" style={{ width: '100%' }}/>
+          </td></tr>
+      
+          </tbody></table> : <div></div>);
+          
         }
 
 
 
   return(
-    <ScheduleComponent width='100%' height='550px' currentView='Week' selectedDate={new Date(2022, 11, 1)}  group={{ resources: ['Owners'] }} editorTemplate={editorTemplate.bind(this)}>
+    <ScheduleComponent width='100%' height='550px' currentView='Week' selectedDate={new Date(2022, 11, 1)} group={{ resources: ['Owners'] }} showQuickInfo={false} editorTemplate={editorTemplate.bind(this)} actionBegin={onActionBegin.bind(this)}>
       <ViewsDirective>
           <ViewDirective option='Day'/>
           <ViewDirective option='Week'/>
       </ViewsDirective>
-      <Editing allowAdding={true}/>
       <ResourcesDirective>
-          <ResourceDirective field='OwnerId' title='Owner' name='Owners' allowMultiple={true} dataSource={data} textField='DockName' idField='Id'>
-          </ResourceDirective>
+          <ResourceDirective field='OwnerId' title='Owner' name='Owners' allowMultiple={true} dataSource={docks} textField='DockName' idField='Id'> </ResourceDirective>
       </ResourcesDirective>
       <Inject services={[Day, Week, TimelineViews, TimelineMonth]}/>
     </ScheduleComponent>
--- src/pages/Home/useData.js
+++ src/pages/Home/useData.js
@@ -1,14 +1,17 @@
 import axios from "axios";
 import React, {useEffect, useState} from "react";
 
-const API_URL = "https://localhost:7034/api/Docks";
+const API_URL = "https://localhost:7034/api/Home";
+
 
 const useData = () => {
 
 const[data, setData] = useState([]);
 
+
+
 const getData = () =>{
-    axios.get(`${API_URL}/getDocks`)
+    axios.get(`${API_URL}/getHomeData`)
         .then((json)=> {
             console.log(json)
             setData(json.data)
@@ -17,8 +20,8 @@ const getData = () =>{
         console.log(error);
         })
    }
-
-   useEffect(()=> getData(), [])
+   
+   useEffect(()=> getData(),[])
    return{data}
 }
 
