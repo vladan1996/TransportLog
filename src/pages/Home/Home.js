a741d5bb9a53cb6314a11d2e16323883f60cfec1
Vladan Duronjic <48985722+vladan1996@users.noreply.github.com>
2022-12-08 15:34:32 +0100

    Add files via upload

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -7,6 +7,7 @@ import { DateTimePickerComponent } from '@syncfusion/ej2-react-calendars';
 import { MultiSelectComponent } from '@syncfusion/ej2-react-dropdowns';
 import {L10n} from '@syncfusion/ej2-base';
 import {insertJob,deleteJob, updateJob} from './homeService';
+import { parseDate } from 'devextreme/localization';
 
 
 
@@ -28,28 +29,43 @@ console.log(statuses);
    const fieldsCustomer = { value: 'Id', text: 'FirstName'};
    const fieldsStatuses = { value: 'Id', text: 'Name' };
 
+   function dateConverter(str){
+
+    var date = new Date(str);
+    const mnth = ("0" + (date.getMonth()+1)).slice(-2);
+    const day  = ("0" + date.getDate()).slice(-2);
+    const hours  = ("0" + date.getHours()).slice(-2);
+    const minutes = ("0" + date.getMinutes()).slice(-2);
+    const seconds  = ("0" + date.getSeconds()).slice(-2);
+    const year = date.getFullYear();
+    return `${year}/${mnth}/${day} ${hours}:${minutes}:${seconds}`
+ }
+
+
+
+
 
 function onActionBegin(args){
   console.log(args);
 
   if(args.requestType === 'eventCreate') {
 
+    const timeStart = dateConverter(args.addedRecords[0].StartTime.toString());
+
+      console.log(typeof(timeStart));
+    
+
     const job = {
       CustomerId: args.addedRecords[0].CustomerId[0],
-      SuportStatusId: args.addedRecords[0].StatusId[0],
+      SupportStatusesId: args.addedRecords[0].StatusId[0],
       DockId: args.addedRecords[0].OwnerId,
-      endTime : args.addedRecords[0].EndTime,
-      startTime: args.addedRecords[0].StartTime,
+      EndTime : args.addedRecords[0].EndTime,
+      StartTime: timeStart,
       NoPallets: parseInt(args.addedRecords[0].NoPallets),
       LoadNo: args.addedRecords[0].LoadNo,
       LoadType: args.addedRecords[0].LoadType,
-      Customers: [],
-      SupportStatuses: [],
-      Dock: [],
-      job:[]
     }
     insertJob(job);
-    console.log(args);
   }else if(args.requestType === 'eventRemove'){
     deleteJob(args.deletedRecords[0].Guid);
   }
@@ -59,17 +75,17 @@ function onActionBegin(args){
 
  function editorTemplate(props) {
   console.log(props)
+
   
        return (props !== undefined && Object.keys(props).length > 0 ? 
        <table className="custom-event-editor" style={{ width: '100%', padding: '5' }}><tbody>
               <tr><td className="e-textlabel">Start Date</td><td colSpan={4}>
-            <DateTimePickerComponent format='dd/MM/yy hh:mm' id="StartTime" data-name="StartTime" className="e-field"></DateTimePickerComponent>
-                    {/* value={new Date(props.startTime || props.StartTime)} */}
+                
+            <DateTimePickerComponent format='yyyy-MM-dd HH:mm' id="StartTime" data-name="StartTime" className="e-field" value={new Date(props.startTime || props.StartTime)}></DateTimePickerComponent>
           </td></tr>
 
           <tr><td className="e-textlabel">End Date</td><td colSpan={4}>
-            <DateTimePickerComponent format='dd/MM/yy hh:mm' id="EndTime" data-name="EndTime" value={new Date(props.endTime || props.EndTime)} className="e-field"></DateTimePickerComponent>
-         {/* value={new Date(props.endTime || props.EndTime)} */}
+            <DateTimePickerComponent format='dd/MM/yyyy hh:mm a' id="EndTime" data-name="EndTime" value={new Date(props.endTime || props.EndTime)} className="e-field"></DateTimePickerComponent>
           </td></tr>
 
           <tr><td className="e-textlabel">Dock</td><td colSpan={4}>
@@ -116,59 +132,3 @@ function onActionBegin(args){
   );  
 }
 export default Home;
-
-
-
-
-// import * as ReactDOM from 'react-dom';
-// import * as React from 'react';
-// import { ScheduleComponent, ViewsDirective, ViewDirective, Day, Week, WorkWeek, Month, ResourcesDirective, ResourceDirective, Inject } from '@syncfusion/ej2-react-schedule';
-// import { extend } from '@syncfusion/ej2-base';
-// import { DateTimePickerComponent } from '@syncfusion/ej2-react-calendars';
-// import { MultiSelectComponent } from '@syncfusion/ej2-react-dropdowns';
-// class App extends React.Component {
-//     constructor() {
-//         super(...arguments);
-//         this.eventSettings = { dataSource: this.data };
-//         this.ownerData = [
-//             { OwnerText: 'Nancy', Id: 1, OwnerColor: '#ffaa00' },
-//             { OwnerText: 'Steven', Id: 2, OwnerColor: '#f8a398' },
-//             { OwnerText: 'Michael', Id: 3, OwnerColor: '#7499e1' }
-//         ];
-//         this.fields = { text: 'OwnerText', value: 'Id' };
-//     }
-//     editorTemplate(props) {
-//         return (props !== undefined && Object.keys(props).length > 0 ? <table className="custom-event-editor" style={{ width: '100%', padding: '5' }}><tbody>
-//       <tr><td className="e-textlabel">Summary</td><td colSpan={4}>
-//         <input id="Summary" className="e-field e-input" type="text" name="Subject" style={{ width: '100%' }}/>
-//       </td></tr>
-//       <tr><td className="e-textlabel">Owner</td><td colSpan={4}>
-//         <MultiSelectComponent className="e-field" placeholder='Choose owner' data-name="OwnerId" dataSource={this.ownerData} fields={this.fields} value={props.OwnerId}/>
-//       </td></tr>
-//       <tr><td className="e-textlabel">From</td><td colSpan={4}>
-//         <DateTimePickerComponent format='dd/MM/yy hh:mm a' id="StartTime" data-name="StartTime" value={new Date(props.startTime || props.StartTime)} className="e-field"></DateTimePickerComponent>
-//       </td></tr>
-//       <tr><td className="e-textlabel">To</td><td colSpan={4}>
-//         <DateTimePickerComponent format='dd/MM/yy hh:mm a' id="EndTime" data-name="EndTime" value={new Date(props.endTime || props.EndTime)} className="e-field"></DateTimePickerComponent>
-//       </td></tr>
-//       <tr><td className="e-textlabel">Reason</td><td colSpan={4}>
-//         <textarea id="Description" className="e-field e-input" name="Description" rows={3} cols={50} style={{ width: '100%', height: '60px !important', resize: 'vertical' }}></textarea>
-//       </td></tr></tbody></table> : <div></div>);
-//     }
-//     render() {
-//         return <ScheduleComponent width='100%' height='550px' selectedDate={new Date(2018, 1, 15)} ref={schedule => this.scheduleObj = schedule} eventSettings={this.eventSettings} editorTemplate={this.editorTemplate.bind(this)} showQuickInfo={false} group={{ resources: ['Owners'] }}>
-//       <ResourcesDirective>
-//         <ResourceDirective field='OwnerId' title='Owner' name='Owners' allowMultiple={false} dataSource={this.ownerData} textField='OwnerText' idField='Id' allowGroupEdit={false} colorField='OwnerColor'></ResourceDirective>
-//       </ResourcesDirective>
-//       <ViewsDirective>
-//         <ViewDirective option='Day'/>
-//         <ViewDirective option='Week'/>
-//         <ViewDirective option='WorkWeek'/>
-//         <ViewDirective option='Month'/>
-//       </ViewsDirective>
-//       <Inject services={[Day, Week, WorkWeek, Month]}/>
-//     </ScheduleComponent>;
-//     }
-// }
-// ;
-// ReactDOM.render(<App />, document.getElementById('schedule'));
